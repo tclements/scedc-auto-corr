7506cd99753e6ac4ba45494189e0f6ee9507867a
Tim Clements <thclements@g.harvard.edu>
2020-07-07 16:55:08 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.5

---
+++ src/SCEDCAutoCorr.jl
@@ -0,0 +1,5 @@
+module SCEDCAutoCorr
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SCEDCAutoCorr
+using Test
+
+@testset "SCEDCAutoCorr.jl" begin
+    # Write your tests here.
+end
