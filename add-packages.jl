1b7a02fc6bd5fd2931afe55dda0c051e02530f7e
Tim Clements <thclements@g.harvard.edu>
2020-07-07 17:21:44 -0400

    Remove old files

--- add-packages.jl
+++
@@ -1,7 +0,0 @@
-using Pkg
-
-# add following packages
-Pkg.add(["AWSCore","AWSS3","DataFrames", "DSP", "FFTW", "Glob", "JLD2", 
-       "Interpolations", "GLM", "Plots","CSV"])
-Pkg.add(PackageSpec(name="SeisIO", rev="master"))
-Pkg.add(PackageSpec(name="SeisNoise", rev="master"))
