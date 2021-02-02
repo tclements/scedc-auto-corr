5c6ec9bfd813dc492d59d0179e602af070d83c62
tclements <thclements@g.harvard.edu>
2021-02-02 11:13:45 -0500

    update to new SeisIO version

--- src/SCEDCCorr.jl
+++ src/SCEDCCorr.jl
@@ -1,6 +1,6 @@
 module SCEDCCorr
 
-using SeisIO, SeisNoise, AWSS3, AWSCore, Dates, Glob, Statistics, JLD2
+using SeisIO, SeisNoise, AWSS3, AWS, Dates, Glob, Statistics, JLD2
 using CSV, DataFrames
 include("tools.jl")
 include("autocorr.jl")
